aff2396f2f5c756dc2e08dd80106f12c0215ea76
Antonypineda12 <90173896+Antonypineda12@users.noreply.github.com>
2021-09-06 13:55:46 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+})
+
+var inputs = document.getElementsByClassName('formulario_input-text')
+for (var i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener('keyup', function(){
+        if(this.value.length>=1){
+                this.nextElementSibling.classList.add('fijar');
+        } else {
+            this.nextElementSibling.classList.remove('fijar')
+        }
+    })
+}
+var inputs = document.getElementsByClassName('formulario_input-text')
